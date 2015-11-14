af99f43951e94428aba33600e120e052b1f9ef02
davidnicholai <nicholai.david@gmail.com>
2015-11-14 17:58:06 +0800

    UI modifications 2

--- app/controllers/carts.server.controller.js
+++ app/controllers/carts.server.controller.js
@@ -362,10 +362,10 @@ exports.checkout = function (req, res) {
         };
 
         paypalPayment.transactions[0].amount.total = totalPrice;
-        // paypalPayment.redirect_urls.return_url = 'https://104.236.16.2/#!/shop/cart/confirm';
-        // paypalPayment.redirect_urls.cancel_url = 'https://104.236.16.2/#!/shop/cart';
-        paypalPayment.redirect_urls.return_url = 'http://localhost:3000/#!/shop/cart/confirm';
-        paypalPayment.redirect_urls.cancel_url = 'http://localhost:3000/#!/shop/cart/cancel';
+        paypalPayment.redirect_urls.return_url = 'https://104.236.16.2/#!/shop/cart/confirm';
+        paypalPayment.redirect_urls.cancel_url = 'https://104.236.16.2/#!/shop/cart';
+        // paypalPayment.redirect_urls.return_url = 'http://localhost:3000/#!/shop/cart/confirm';
+        // paypalPayment.redirect_urls.cancel_url = 'http://localhost:3000/#!/shop/cart/cancel';
         paypalPayment.transactions[0].description = 'Total Price: $' + totalPrice;
         
         paypal.payment.create(paypalPayment, {}, function (err, response) {
